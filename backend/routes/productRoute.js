8394d4bde814655f817f006e5e5a774b11c6c361
Malachi Ezeabuchukwu <angelmalachi5@gmail.com>
2023-11-14 18:06:46 +0100

    update

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {
+  createProduct,
+  getaProduct,
+  getAllProduct,
+  updateProduct,
+  deleteProduct,
+//   addToWishlist,
+//   rating,
+} = require("../controller/productCtrl");
+// const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+const router = express.Router();
+
+router.post("/", createProduct);
+
+router.get("/:id", getaProduct);
+// router.put("/wishlist", authMiddleware, addToWishlist);
+// router.put("/rating", authMiddleware, rating);
+
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+router.get("/", getAllProduct);
+
+module.exports = router;
